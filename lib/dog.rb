333d51ef1ac469c0375ac880377c2cc470ee8840
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-17 02:37:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,11 +7,14 @@ class Dog
     @@all << self
   end
 
-  def self.all
-    @@all
+  def self.all 
+    @@all 
   end
 
+  def self.clear_all
+  end
 
-
-
+  def self.print_all
+  end
+  
 end# Add your code here
